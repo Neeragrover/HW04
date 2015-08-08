05abb90ebefaa9e667475f10bfaae76d6ee10b61
Neeragrover <maverick.neera@gmail.com>
2015-08-07 17:03:58 -0700

    Guessing numbers exercise

--- HW04_ex00.py
+++ HW04_ex00.py
@@ -11,19 +11,38 @@
 #         - then ends the program
 ################################################################################
 # Imports
-
+import random
 
 # Body
 
-
-
-
 ################################################################################
 def main():
+	print("Hello World!") # Remove this and replace with your function calls
+	turn=0	
+	x=random.randint(1,25)
+	
+	try:
+		while(turn<5):
+			input_num=raw_input('enter a number:')
+			input_num_int=int(input_num)
+			if(input_num_int==x):
+				print "Congratulations, you got it!"
+				break
+			elif (input_num_int<x):
+				print "Too Low, please guess again!"
+				turn=turn+1
+			elif (input_num_int>x):
+				print "Too High,please guess again!"
+				turn=turn+1
+			else:
+				print 'random'
+							
+			if turn==5:		
+				print "Sorry, you ran out of turns. Please start over to guess again!!"
+	except:
+		print "Only numbers please, start over buddy!"
 
-
-    print("Hello World!") # Remove this and replace with your function calls
-    
+		
 
 if __name__ == '__main__':
     main()
